e0a9d47d93b32c31b4ec8c89000c1de21a628548
Cristiano Scandura <53795829+scandura@users.noreply.github.com>
2022-08-29 16:28:20 -0300

    Fix MS Windows user creation

---
+++ servicecatalog-templates/windows-user-data.ps1
@@ -0,0 +1,33 @@
+$logFile = "C:\log.txt"
+
+$ssmKey = $args[0]
+
+$password = & "C:\Program Files\Amazon\AWSCLIV2\aws.exe"  ssm get-parameter --name "$ssmKey" --region sa-east-1 --with-decryption --query "Parameter.Value" --output text
+
+$securePass = ConvertTo-SecureString $password -AsPlainText -Force
+
+Function Write-Log {
+  param(
+      [Parameter(Mandatory = $true)][string] $message,
+      [Parameter(Mandatory = $false)]
+      [ValidateSet("INFO","WARN","ERROR")]
+      [string] $level = "INFO"
+  )
+  # Create timestamp
+  $timestamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+  # Append content to log file
+  Add-Content -Path $logFile -Value "$timestamp [$level] - $message"
+}
+
+Function Create-LocalAdmin {
+    process {
+      try {
+        New-LocalUser "developer" -Password $securePass -FullName "Developer" -Description "Developer Account"
+        Add-LocalGroupMember -Group "Administrators" -Member "developer"
+      }catch{
+    Write-log -message "Creating local account failed" -level "ERROR"
+  }
+}
+}
+
+Create-LocalAdmin
